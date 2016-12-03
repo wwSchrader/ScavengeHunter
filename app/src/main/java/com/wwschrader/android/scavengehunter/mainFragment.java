c0ff3263e5f303563fa23c40ad112171ce6b4128
Warren Schrader <wwschrader@gmail.com>
2016-12-03 15:29:55 -0800

    Add main fragment class and layout

---
+++ app/src/main/java/com/wwschrader/android/scavengehunter/mainFragment.java
@@ -0,0 +1,10 @@
+package com.wwschrader.android.scavengehunter;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Warren on 12/2/2016.
+ */
+
+public class MainFragment extends AppCompatActivity {
+}
