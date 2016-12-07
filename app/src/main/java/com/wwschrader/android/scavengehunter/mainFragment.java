5de7194e2286fb1db1428fc01fb1ef02bdba580a
Warren Schrader <wwschrader@gmail.com>
2016-12-06 19:15:11 -0800

    Add main fragment with textview and buttons

--- app/src/main/java/com/wwschrader/android/scavengehunter/mainFragment.java
+++ app/src/main/java/com/wwschrader/android/scavengehunter/mainFragment.java
@@ -1,10 +1,29 @@
 package com.wwschrader.android.scavengehunter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Warren on 12/2/2016.
  */
 
-public class MainFragment extends AppCompatActivity {
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        return view;
+    }
+
+    //user selects to create a hunt
+    public void createHunt(View view){
+
+    }
 }
