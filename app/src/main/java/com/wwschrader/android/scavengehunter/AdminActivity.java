55170c75d02fdbd3dcfa482c02eef13c22e6b08a
Warren Schrader <wwschrader@gmail.com>
2016-12-09 18:57:10 -0800

    Add navigation drawer

--- app/src/main/java/com/wwschrader/android/scavengehunter/AdminActivity.java
+++ app/src/main/java/com/wwschrader/android/scavengehunter/AdminActivity.java
@@ -1,10 +1,14 @@
 package com.wwschrader.android.scavengehunter;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.wwschrader.android.scavengehunter.adapters.CollectionPagerAdapter;
 
@@ -14,11 +18,26 @@ import com.wwschrader.android.scavengehunter.adapters.CollectionPagerAdapter;
  */
 
 public class AdminActivity extends AppCompatActivity {
+    @SuppressWarnings("FieldCanBeLocal")
+    private NavigationView mNavigationView;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_pager);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setCheckable(true);
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         CollectionPagerAdapter collectionPagerAdapter = new CollectionPagerAdapter(getSupportFragmentManager(), this);
