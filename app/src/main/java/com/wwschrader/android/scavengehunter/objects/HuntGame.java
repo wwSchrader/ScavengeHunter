6b4bbc2ed1b9d1c2c947d0327dec46ad626e0981
Warren Schrader <wwschrader@gmail.com>
2016-12-13 18:00:29 -0800

    Add hunt object

---
+++ app/src/main/java/com/wwschrader/android/scavengehunter/objects/HuntGame.java
@@ -0,0 +1,54 @@
+package com.wwschrader.android.scavengehunter.objects;
+
+/**
+ * Created by Warren on 12/13/2016.
+ * Object representing hunt games to be synced on Firebase
+ */
+
+public class HuntGame {
+
+    private String huntName;
+    private String huntPassword;
+    private long huntStartTime;
+    private long huntEndingTime;
+
+    public String getHuntName() {
+        return huntName;
+    }
+
+    public void setHuntName(String huntName) {
+        this.huntName = huntName;
+    }
+
+    public String getHuntPassword() {
+        return huntPassword;
+    }
+
+    public void setHuntPassword(String huntPassword) {
+        this.huntPassword = huntPassword;
+    }
+
+    public long getHuntStartTime() {
+        return huntStartTime;
+    }
+
+    public void setHuntStartTime(long huntStartTime) {
+        this.huntStartTime = huntStartTime;
+    }
+
+    public long getHuntEndingTime() {
+        return huntEndingTime;
+    }
+
+    public void setHuntEndingTime(long huntEndingTime) {
+        this.huntEndingTime = huntEndingTime;
+    }
+
+    public HuntGame(String huntName, String huntPassword, long huntStartTime, long huntEndingTime) {
+
+        this.huntName = huntName;
+        this.huntPassword = huntPassword;
+        this.huntStartTime = huntStartTime;
+        this.huntEndingTime = huntEndingTime;
+    }
+}
