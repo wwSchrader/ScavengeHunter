e8b3610d1f4fb6f12aff95e80be6fe775cab1257
Warren Schrader <wwschrader@gmail.com>
2016-12-21 15:38:12 -0800

    Add recycler view for objectives

--- app/src/main/java/com/wwschrader/android/scavengehunter/AdminActivityManagementFragment.java
+++ app/src/main/java/com/wwschrader/android/scavengehunter/AdminActivityManagementFragment.java
@@ -1,26 +1,60 @@
 package com.wwschrader.android.scavengehunter;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.wwschrader.android.scavengehunter.objects.HuntObjectives;
+import com.wwschrader.android.scavengehunter.viewholders.ObjectiveRecyclerViewHolder;
 
 /**
  * Created by Warren on 12/9/2016.
  * Fragment for adding, modifying, or deleting hunt activities
  */
 
+@SuppressWarnings("FieldCanBeLocal")
 public class AdminActivityManagementFragment extends Fragment {
-    FloatingActionButton addActivityActionButton;
+
+    private FloatingActionButton addActivityActionButton;
+    private RecyclerView mRecyclerView;
+    private DatabaseReference mDatabaseReference;
+    private DatabaseReference mObjectiveReference;
+    private FirebaseRecyclerAdapter mAdapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_admin_activity_management, container, false);
+
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.objective_recycler_view);
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        mObjectiveReference = mDatabaseReference.child("objectives");
+        mAdapter = new FirebaseRecyclerAdapter<HuntObjectives, ObjectiveRecyclerViewHolder>(
+                HuntObjectives.class,
+                R.layout.view_holder_objectives,
+                ObjectiveRecyclerViewHolder.class,
+                mObjectiveReference) {
+            @SuppressLint("SetTextI18n")
+            @Override
+            protected void populateViewHolder(ObjectiveRecyclerViewHolder viewHolder, HuntObjectives model, int position) {
+                viewHolder.objectiveNameTextView.setText(model.getObjectiveName());
+                viewHolder.objectiveDescriptionTextView.setText(model.getObjectiveDescription());
+                viewHolder.objectivePointsTextView.setText(Integer.toString(model.getPoints()));
+            }
+        };
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(mAdapter);
 
         addActivityActionButton = (FloatingActionButton) rootView.findViewById(R.id.activities_add_fab);
         addActivityActionButton.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/wwschrader/android/scavengehunter/viewholders/ObjectiveRecyclerViewHolder.java
@@ -0,0 +1,25 @@
+package com.wwschrader.android.scavengehunter.viewholders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.wwschrader.android.scavengehunter.R;
+
+/**
+ * Created by Warren on 12/20/2016.
+ * View holder for objectives in recycler view
+ */
+
+public class ObjectiveRecyclerViewHolder extends RecyclerView.ViewHolder {
+    public final TextView objectiveNameTextView;
+    public final TextView objectiveDescriptionTextView;
+    public final TextView objectivePointsTextView;
+
+    public ObjectiveRecyclerViewHolder(View itemView) {
+        super(itemView);
+        objectiveNameTextView = (TextView) itemView.findViewById(R.id.objective_name_textview);
+        objectiveDescriptionTextView = (TextView) itemView.findViewById(R.id.objective_description_textview);
+        objectivePointsTextView = (TextView) itemView.findViewById(R.id.objective_points_textview);
+    }
+}
